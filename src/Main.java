ceb63e04ce9d986b4d490dde5d92e18205a08969
Bolot Gonchikov <gonchikovbb@gmail.com>
2021-11-26 11:30:52 +0700

    Task 1 - Mili

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        int Bilet = 520;
+        int OneMile_Rub = 20;
+        int Miles = Bilet / OneMile_Rub;
+        System.out.println(Miles);
     }
 }
