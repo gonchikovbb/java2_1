771233c21cd556d199104c9e86295414af614f02
Bolot Gonchikov <gonchikovbb@gmail.com>
2021-11-26 12:57:21 +0700

    Added comments

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int Bilet = 520;
-        int OneMile_Rub = 20;
-        int Miles = Bilet / OneMile_Rub;
+        int Bilet = 520; // Стоимость билета
+        int OneMile_Rub = 20; // Цена одной мили в рублях
+        int Miles = Bilet / OneMile_Rub; // Итого, сколько миль будет начислено
         System.out.println(Miles);
     }
 }
